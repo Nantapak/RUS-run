042c7ed9ad7d80eb0a59f7cb525d589f99ee3c00
Nantapak <nantapak1011@gmail.com>
2016-07-14 11:59:07 +0700

    Authen Finish

--- app/src/main/java/suwanpakdee/nantapak/rusrun/MainActivity.java
+++ app/src/main/java/suwanpakdee/nantapak/rusrun/MainActivity.java
@@ -1,14 +1,23 @@
 package suwanpakdee.nantapak.rusrun;
 
+import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private static final String urlLogo = "http://swiftcodingthai.com/rus/image/logo_rus.png";
     private String userString, passwordString;
+    private static final String urlJSON = "http://swiftcodingthai.com/rus/get_user_master.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +35,101 @@ public class MainActivity extends AppCompatActivity {
 
         //Bind Widget
         userEditText = (EditText) findViewById(R.id.editText4);
-        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
         imageView = (ImageView) findViewById(R.id.imageView6);
 
         //Load Image from Server
         Picasso.with(this).load(urlLogo).into(imageView);
 
     }   // Main Method
+
+    //Create Inner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private String myJSONString, myUserString, myPasswordString;
+        private Context context;
+        private boolean statusABoolean = true;
+        private String truePassword;
+        private String myNameString, myIDString, myAvataString;
+
+        public SynUser(String myJSONString,
+                       String myUserString,
+                       String myPasswordString,
+                       Context context) {
+            this.myJSONString = myJSONString;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(myJSONString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("RusV1", "e doIn ==> " + e.toString());
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("RusV1", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i=0;i<jsonArray.length();i+=1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (myUserString.equals(jsonObject.getString("User"))) {
+
+                        statusABoolean = false;
+                        truePassword = jsonObject.getString("Password");
+                        myNameString = jsonObject.getString("Name");
+                        myIDString = jsonObject.getString("id");
+                        myAvataString = jsonObject.getString("Avata");
+                    }
+                }   //for
+
+                if (statusABoolean) {
+
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "User ไม่ถูกต้อง",
+                            "ไม่มี" + myUserString + "ในฐานข้อมูลของเรา");
+
+                } else if (myPasswordString.equals(truePassword)) {
+                    //Password True
+                    Toast.makeText(context, "Welcome "+ myNameString, Toast.LENGTH_SHORT).show();
+                } else {
+                    //Password False
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "Password False",
+                            "Please Try Again Password False");
+                }
+
+            } catch (Exception e) {
+                Log.d("RusV1", "e onPost ==>" + e.toString());
+            }
+
+        }   // onPost
+
+    }   // SynUser Class
+
+
 
     public void clickSignIn(View view) {
 
@@ -39,17 +138,22 @@ public class MainActivity extends AppCompatActivity {
 
         //Check Space
         if (userString.equals("") || passwordString.equals("")) {
-            //HAve Space
+            //Have Space
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "Have Space",
                     "Please Fill All Every Blank");
+
         } else {
             //No Space
+            SynUser synUser = new SynUser(urlJSON, userString, passwordString, this);
+            synUser.execute();
+
         }   //if
 
-    }   //clickSign
 
 
+
+    }   // clickSign
 
 
     public void clickSignUpMain(View view) {
